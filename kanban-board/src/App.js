c4ccde924ebd3eb280f23c2ea17aff7f6c1ed0b3
vquijano12 <vquijano7224@gmail.com>
2024-12-26 23:23:36 -0500

    chore: format code with Prettier

--- kanban-board/src/App.js
+++ kanban-board/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import KanbanBoard from './components/KanbanBoard';
-import './App.css';
+import React from "react";
+import KanbanBoard from "./components/KanbanBoard";
+import "./App.css";
 
 function App() {
   return (
